fb80961d3f5b6938fc288d02ba46841172a74908
档100 <31099477+shilela@users.noreply.github.com>
2018-06-03 17:22:45 +0800

    Update Trans.java

--- Trans.java
+++ Trans.java
@@ -1,6 +1,4 @@
 public class Trans {
-
-	static final String[] a = {"0","1","2","3","4","5","6","7","8","9"};
 	static final String[] b = {"零","壹","贰","叁","肆","伍","陆","柒","捌","玖"};
 	static final String[] c = {"","圆","拾","佰","仟","万","拾","佰","仟","亿","拾","佰","仟"};
 	public static String trans(String num)
@@ -12,7 +10,7 @@ public class Trans {
 		{
 			for(int i = 1;i <= num.length() ;i++)
 			{
-				trans = b[(int)(num.charAt(num.length() - i))-48] + c[i] + trans;
+				trans = b[(int)(num.charAt(num.length() - i)) - 48] + c[i] + trans;
 			}
 		}
 			return trans;
